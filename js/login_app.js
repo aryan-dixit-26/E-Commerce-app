fd8bf4885cd80ca2652d706bdb44a28f1b6f498b
anushka141 <61308296+anushka141@users.noreply.github.com>
2021-08-05 20:59:42 +0530

    Update login_app.js

--- js/login_app.js
+++ js/login_app.js
@@ -3,3 +3,7 @@ const signupbutton = document.querySelector('#sign-up-button');
 signupbutton.addEventListener('click' , () => {
     window.location = "signup.html";
 });
+const homepage = document.querySelector('#login-page-logo img');
+homepage.addEventListener('click' ,()=>{
+    window.location ="index.html";
+});
