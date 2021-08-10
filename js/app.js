9e7e49b56580a7b0bb9f983da2448d009edcda42
anushka141 <61308296+anushka141@users.noreply.github.com>
2021-08-10 23:55:52 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ const loginbutton = document.querySelector('#login-home button');
 loginbutton.addEventListener("click", () => {
     window.location = "login.html";
 });
+
+const payment = document.querySelector('#cart-home-page img');
+payment.addEventListener("click", ()=> {
+    window.location = "cart.html";
+});
